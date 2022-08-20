78bb7c2159e29096a1ab8c37f9d752a0f3c6d6b7
ArnoDekkersLos <a_dekkerslos@hotmail.com>
2022-08-20 16:18:24 +0200

    Bugfix for the buffer Exception

--- Challenge5-solana/src/App.tsx
+++ Challenge5-solana/src/App.tsx
@@ -9,6 +9,9 @@ import { createPrivateKey, createPublicKey } from './lib/keyPairs';
 import { airdropSOL, getSOLBalance, transferSOL } from './lib/wallet';
 import { Keypair, } from '@solana/web3.js';
 import { convertToLamports } from './lib/lamport';
+// Magical statement copied from MinG#7128, long may he reign
+// this fixes the `ReferenceError: Buffer is not defined` when transferring SOL
+window.Buffer = window.Buffer || require('buffer').Buffer;
 
 function App() {
   // create state variable for the provider
